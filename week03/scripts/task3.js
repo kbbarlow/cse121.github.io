604902e81fac9d4bd134e122b62efb3c09a7ce10
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-11 21:26:14 -0600

    .

--- week03/scripts/task3.js
+++ week03/scripts/task3.js
@@ -4,18 +4,21 @@
 
 // Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
 function add(number1, number2) {
+    
   // Step 2: In the function, return the sum of the parameters number1 and number2
   return number1 + number2;
 }
-// Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
-function addNumbers() {
-  let append1 = parseInt(document.querySelector('#append1').value);
-  let append2 = parseInt(document.querySelector('#append2').value);
-  let result = add(append1, append2);
 
-  // Step 4: Assign the return value to an HTML form element with an ID of sum
+// Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
+function addNumbers() {
+  let addend1 = parseInt(document.querySelector('#addend1').value);
+  let addend2 = parseInt(document.querySelector('#addend2').value);
+  let result = add(addend1, addend2);
+
+    // Step 4: Assign the return value to an HTML form element with an ID of sum
   document.querySelector('#sum').value = result;
 }
+
 // Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 // Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
