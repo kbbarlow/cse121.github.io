69a7d6b15a263efec1222125c0082b22fad55b32
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-18 23:27:22 -0600

    .

--- week05/scripts/task5.js
+++ week05/scripts/task5.js
@@ -3,32 +3,32 @@
 /* IF/ELSE IF */
 
 // Step 1: Declare and initialize a new variable to hold the current date
-var date = new Date();
+const date = new Date();
 console.log(date)
 
 // Step 2: Declare another variable to hold the day of the week
-var dayofweek = date.getDay();
+let dayofweek = date.getDay();
 
 // Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
 
 // Step 4: Declare a variable to hold a message that will be displayed
-var message = '';
+let message1;
 
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
-if (dayofweek >1 || dayofweek<6){
-  message="Hang in there!"
+if (dayofweek >=1 || dayofweek<=5){
+  message1="Hang in there!"
 }
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
 else {
-  message="Woohoo!  It is the weekend!"
+  message1="Woohoo!  It is the weekend!"
 }
 
 /* SWITCH, CASE, BREAK */
 
 // Step 1: Declare a new variable to hold another message
-var message2 = '';
+var message2;
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
-switch (new Date().getDay()) {
+switch (dayofweek) {
   case 0:
     day = "Sunday";
     break;
@@ -54,7 +54,7 @@ switch (new Date().getDay()) {
 /* OUTPUT */
 
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
-document.querySelector('#message1').textContent =message;
+document.querySelector('#message1').textContent =message1;
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
 document.querySelector('#message2').textContent =message2;
 
