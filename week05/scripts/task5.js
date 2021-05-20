b088b086d06d0f981e60dcb3121689ca460dc695
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-20 16:47:25 -0600

    .

--- week05/scripts/task5.js
+++ week05/scripts/task5.js
@@ -14,12 +14,12 @@ let dayofweek = date.getDay();
 let message1;
 
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
-if (dayofweek >=1 && dayofweek<=5){
-  message1="Hang in there!"
+if (dayofweek >= 1 && dayofweek <= 5) {
+  message1 = "Hang in there!";
 }
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
 else {
-  message1="Woohoo!  It is the weekend!"
+  message1 = "Woohoo!  It is the weekend!";
 }
 
 /* SWITCH, CASE, BREAK */
@@ -50,16 +50,16 @@ switch (dayofweek) {
     message2 = "Saturday";
     break;
   default:
-    message2 = 'Unknown - ' + dayOfWeek;
+    message2 = "Unknown - " + dayOfWeek;
     break;
 }
 
 /* OUTPUT */
 
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
-document.querySelector('#message1').textContent =message1;
+document.querySelector("#message1").textContent = message1;
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
-document.querySelector('#message2').textContent =message2;
+document.querySelector("#message2").textContent = message2;
 
 /* FETCH */
 
@@ -72,34 +72,31 @@ document.querySelector('#message2').textContent =message2;
 // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of temples
-let templeList =[];
+let templeList = [];
 const output = (temples) => {
-  temples.forEach(
-    temple => {
-      let article = document.createElement("article");
-      
-      let templeName = document.createElement("h3");
-      templeName.textContent=temple.templeName;
-      
-      let location = document.createElement("h4");
-      location.textContent=temple.location;
-      
-      let dedicated = document.createElement("h4");
-      dedicated.textContent = temple.dedicated;
-      
-      let image = document.createElement("img");
-      image.setAttribute('src', temple.imageUrl);
-      image.setAttribute('alt', temple.templeName);
-      
-      article.appendChild(templeName);
-      article.appendChild(location);
-      article.appendChild(dedicated);
-      article.appendChild(image);
-      document.querySelector("#temples").appendChild(article);
-    }
-  );
-}
+  temples.forEach((temple) => {
+    let article = document.createElement("article");
 
+    let templeName = document.createElement("h3");
+    templeName.textContent = temple.templeName;
+
+    let location = document.createElement("h4");
+    location.textContent = temple.location;
+
+    let dedicated = document.createElement("h4");
+    dedicated.textContent = temple.dedicated;
+
+    let image = document.createElement("img");
+    image.setAttribute("src", temple.imageUrl);
+    image.setAttribute("alt", temple.templeName);
+
+    article.appendChild(templeName);
+    article.appendChild(location);
+    article.appendChild(dedicated);
+    article.appendChild(image);
+    document.querySelector("#temples").appendChild(article);
+  });
+};
 
 // Step 3: Using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'
 
@@ -111,58 +108,64 @@ const output = (temples) => {
 
 // Step 7: Finally, call the output function and pass it the list of temples
 
-// Step 8: Declare a function named reset that clears all of the <article> 
+// Step 8: Declare a function named reset that clears all of the <article>
 //elements from the HTML element with an ID of temples
 
-
-fetch ('https://byui-cse.github.io/cse121b-course/week05/temples.json')
-  .then(response=> response.json())
-  .then(temples=>{
-    templeList =temples;
+fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json")
+  .then((response) => response.json())
+  .then((temples) => {
+    templeList = temples;
     output(templeList);
   });
-const reset = ()=>{
-  document.querySelector('#temples').innerHTML = "";
-}
+const reset = () => {
+  document.querySelector("#temples").innerHTML = "";
+};
 // Step 9: Declare a function named sortBy that does the following:
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
 // - Calls the output function passing in the sorted list of temples
 // Step 10: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
-const sortBy = ()=>{
+const sortBy = () => {
   reset();
   let filterlist = document.querySelector("#sortBy").value;
-  switch(filterlist){
-    case 'templeNameAscending':
-      output(templeList.sort(
-        (temple1, temple2)=>{
+  switch (filterlist) {
+    case "templeNameAscending":
+      output(
+        templeList.sort((temple1, temple2) => {
           let templeName1 = temple1.templeName.toLowerCase();
           let templeName2 = temple2.templeName.toLowerCase();
-          if(templeName1 < templeName2) return -1;
-          else if (templeName1>templeName2) return 1;
+          if (templeName1 < templeName2) return -1;
+          else if (templeName1 > templeName2) return 1;
           else return 0;
-        }));
+        })
+      );
       break;
-    case 'templeNameDescending':
-      output(templeList.sort(
-        (temple1, temple2)=>{
+    case "templeNameDescending":
+      output(
+        templeList.sort((temple1, temple2) => {
           let templeName1 = temple1.templeName.toLowerCase();
           let templeName2 = temple2.templeName.toLowerCase();
-          if(templeName1 > templeName2) return -1;
+          if (templeName1 > templeName2) return -1;
           else if (templeName1 < templeName2) return 1;
           else return 0;
-       }));
-     break; 
-        
-    default:
-      output(templeList.sort(
-        (temple1, temple2)=>
-          temple1.templeName.toLowerCase() > temple2.templeName.toLowerCase() ? 1:
-            temple2.templeNametoLowerCase() > temple1.templeName.toLowerCase() ? -1: 0))
+        })
+      );
       break;
-    }
+
+    default:
+      output(
+        templeList.sort((temple1, temple2) =>
+          temple1.templeName.toLowerCase() > temple2.templeName.toLowerCase()
+            ? 1
+            : temple2.templeNametoLowerCase() > temple1.templeName.toLowerCase()
+            ? -1
+            : 0
+        )
+      );
+      break;
   }
-document.querySelector('#sortBy').addEventListener('change', sortBy);
+};
+document.querySelector("#sortBy").addEventListener("change", sortBy);
 
 /* STRETCH */
 
