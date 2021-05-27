efd4d91ee197eb3620223b59eba15ab9ac27dfa1
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-26 21:05:50 -0600

    .

--- week06/scripts/joke.js
+++ week06/scripts/joke.js
@@ -1,10 +1,14 @@
 var unirest = require("unirest");
 
-var req = unirest("GET", "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random");
+var req = unirest("GET", "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/search");
+
+req.query({
+	"query": "<REQUIRED>"
+});
 
 req.headers({
 	"accept": "application/json",
-	"x-rapidapi-key": "SIGN-UP-FOR-KEY",
+	"x-rapidapi-key": "879a0f6dc9msh2a93d4cf3d0176dp154fd7jsn5faee78b95d1",
 	"x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
 	"useQueryString": true
 });
