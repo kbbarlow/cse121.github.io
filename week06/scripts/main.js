fe74f7d87b895fb03f5c117ac8f974c13c6dbe96
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-26 23:54:07 -0600

    .

--- week06/scripts/main.js
+++ week06/scripts/main.js
@@ -26,6 +26,7 @@ for (let i = 0; i < localStorage.length; i++){
   sum += parseInt(valu[0]); 
   output.innerHTML +=` ${key}: ${value} minutes <br />`;
 }
+var hoursleft = 40 - (sum / 60);
 document.getElementById("time").innerHTML = `Total Minutes: ${sum}`
-document.getElementById("hoursleft").innerHTML = `Total Hours: ${sum/60} Hours Left : ${40(sum/60)}`
+document.getElementById("hoursleft").innerHTML = `Total Hours: ${sum/60} Hours Left : ${hoursleft}`
 
