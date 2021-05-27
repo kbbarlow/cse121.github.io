5b87b0d99eeca4fdd19305a7aeca74ea2d05f201
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-26 22:55:36 -0600

    .

--- week06/scripts/weather.js
+++ week06/scripts/weather.js
@@ -12,17 +12,17 @@ fetch(APIurl)
     document.getElementById('current-temp').innerHTML = Math.round(jsObject.current.temp) + "&#8457;";
     document.getElementById('feels_like').innerHTML = Math.round(jsObject.current.feels_like) + "&#8457;";
     document.getElementById('humidity').innerHTML = Math.round(jsObject.current.humidity) + "&#37;";
-    var forecast = jsObject.daily.slice(1, 4);
-    const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    for (let day = 0; day < forecast.length; day++) {
-      const d = new Date(forecast[day].dt * 1000); // The new Date() function needs a milliseconds value. Convert Unix timestamp to milliseconds by multiplying by 1000.
-      const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
-      const desc = forecast[day].weather[0].description;
-      document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
-      document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].temp.max);
-      document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-      document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
-    }
+    // var forecast = jsObject.daily.slice(1, 4);
+    // const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    // for (let day = 0; day < forecast.length; day++) {
+    //   const d = new Date(forecast[day].dt * 1000); // The new Date() function needs a milliseconds value. Convert Unix timestamp to milliseconds by multiplying by 1000.
+    //   const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
+    //   const desc = forecast[day].weather[0].description;
+    //   document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+    //   document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].temp.max);
+    //   document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+    //   document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+    // }
 //     if (jsObject.hasOwnProperty('alerts')) {
 //       document.querySelector('.weatheralert').style.display = 'block';
 //       document.getElementById('alert').innerHTML = jsObject.alerts[0].event;
