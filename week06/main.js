1e7109f60f668cf648e5adf06325fc7d6c6fa0cd
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-26 18:18:34 -0600

    .

--- week06/main.js
+++ week06/main.js
@@ -1,1 +1,13 @@
-localStorage.setItem("","")
+const inputKey = document.getElementById('inputKey');
+const inputValue = document.getElementById('inputValueinputKey');
+const buttonInsert = document.getElementById('buttonInsert');
+const btnClear = document.getElementById('btnClear');
+const output = document.getElementById('output');
+
+buttonInsert.onclick = function () {
+  const key = inputKey.toLowerCase().value;
+  const value = inputValue.value;
+  console.log(key, value);
+
+};
+
