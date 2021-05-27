4d759dae2ab6bbd10c27dc5bac00fda46cc461e5
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-26 23:33:11 -0600

    .

--- week06/scripts/main.js
+++ week06/scripts/main.js
@@ -15,9 +15,9 @@ buttonInsert.onclick = function () {
     location.reload();
   }
 };
-buttonClear.onclick = function () {
-  localStorage.clear();
-};
+// buttonClear.onclick = function () {
+//   localStorage.clear();
+// };
 var sum = 0;
 for (let i = 0; i < localStorage.length; i++){
   const key = localStorage.key(i);
