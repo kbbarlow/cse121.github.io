9e780c53f29baae799715d2f28c32c5f5ca02ca1
Brooke <71287049+kbbarlow@users.noreply.github.com>
2021-05-26 18:28:25 -0600

    .

--- week06/main.js
+++ week06/main.js
@@ -14,3 +14,9 @@ buttonInsert.onclick = function () {
   }
 };
 
+for (let i = 0; i < localStorage.length; i++){
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
+  output.innerHTML +=`${key}: ${value}<br/>`;
+}
+
